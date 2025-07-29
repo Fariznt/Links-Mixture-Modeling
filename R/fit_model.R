ecdbbfb9d6bc1c0ce99c3dba253d582bb0d0e833
fariznt <fariz.imran98@gmail.com>
2025-07-29 19:42:53 -0400

    undergoing the creation of a replacement function for get_mixture_results. this commit includes both. new one can process parameter stats now. gamma model still broken

--- R/fit_model.R
+++ R/fit_model.R
@@ -50,7 +50,7 @@ fit_model <-
            burning_iterations = 1000, 
            chains = 2, 
            seed = 123) {
-  
+
   validate_args(priors, p_family)
     
   # Set default priors if some or all not passed in
@@ -79,26 +79,24 @@ fit_model <-
     data <- generate_synthetic_mixture_data(p_family, seed)
     print(head(data))  # first few rows of generated data
     print(dim(data))   # dimensions of generated data
-  } else if (is.character(data) && length(data) == 1){
+  } else if (is.character(data) && length(data) == 1) {
     # data is a fie path
     if (!file.exists(data)) {
       stop("Error: The specified CSV file does not exist.")
     }
     data <- read.csv(data)
-      if (anyNA(data)) {
-        na_locations <- which(is.na(data), arr.ind = TRUE)  # Get row and column locations of NA values
-        stop("Error: NA values found in the data. Locations of NA values:\n", 
-            paste("Row:", na_locations[, 1], "Column:", na_locations[, 2], collapse = "\n"))
-      }
-    
-  } else if (is.data.frame(data)) {
-    # already a data.frame — nothing to do
-  
+    if (anyNA(data)) {
+      na_locations <- which(is.na(data), arr.ind = TRUE)  # Get row and column locations of NA values
+      stop("Error: NA values found in the data. Locations of NA values:\n", 
+           paste("Row:", na_locations[, 1], "Column:", na_locations[, 2], collapse = "\n"))
+    }
   } else {
-    stop("`data` must be either:\n",
-         "\"random\"\n",
-         "A single string path to a CSV file\n",
-         "A data.frame")
+    if (!is.data.frame(data) || nrow(data) == 0) {
+      stop("`data` must be either:\n",
+           "\"random\"\n",
+           "A single string path to a CSV file\n",
+           "A nonempty data.frame")
+    }
   }
 
   # Generate stan file
@@ -148,7 +146,6 @@ fit_model <-
     K_per_m = array(K_per_m), # vector of predictors per response var
     X_all = X_all, # col-wise merged matrix of all predictor matrices
     K_sum = K_sum # total num of predictors
-    # TODO: probably possible to remove K_sum, and calculate in stan
   )
 
   # Load the Stan model
@@ -159,8 +156,9 @@ fit_model <-
                   chains = chains, seed = seed, verbose = TRUE)
 
   # Process results based on result_type
-  result <- get_mixture_results(p_family, fit, result_type)
-  return(result)
+  #result <- get_mixture_results(p_family, fit, result_type)
+  #return(result)
+  process_results(p_family, fit, formulas)
 }
 
 
